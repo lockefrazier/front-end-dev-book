4027178f05df635991edf1a57501db1ea7608a02
Locke Frazier <lockefrazier@me.com>
2021-06-19 08:47:48 -0400

    Added functions to get image and title attributes.

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,3 +1,27 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails(imageUrl, titleText) {
+  'use strict';
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute('src', 'imageUrl');
+
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = 'titleText';
+}
+
+function imageFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
